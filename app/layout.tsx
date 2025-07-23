b87599cdd4a4349455f455d22b5d712c6f10c66b
zabriqan <zabriqanzain@gmail.com>
2025-07-24 00:04:47 +0500

    Change toast position

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster />
+        <Toaster position="top-center" />
         <Navbar/>
         <Slider/>
         {children}
