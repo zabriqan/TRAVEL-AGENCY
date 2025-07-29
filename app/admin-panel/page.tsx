efaafe001f0abaefb4a0636fd944690b85302525
Usaid Yousuf <usaidsmart2005@gmail.com>
2025-07-29 16:56:18 +0500

    Fix admin panel route

---
+++ app/admin-panel/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AdminPanelHome() {
+    return (
+        <div>AdminPanelHome</div>
+    )
+}
--- app/admin_panel/page.tsx
+++
@@ -1,5 +0,0 @@
-
-
-export default function adminPage () {
-    return
-}
