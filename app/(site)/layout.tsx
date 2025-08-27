79535480894ad9e16f85117a64b3d40b62380c21
Usaid Yousuf <usaidsmart2005@gmail.com>
2025-08-27 18:49:41 +0500

    Add top loader in site

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from "@/app/components/slider";
 import Footer from "@/app/components/footer";
 import Navbar from "@/app/components/navbar";
+import NextTopLoader from 'nextjs-toploader';
 
 export default function HomeLayout({
     children,
@@ -10,6 +11,7 @@ export default function HomeLayout({
 }>) {
     return (
         <div>
+            <NextTopLoader color='#4CB9C0' height={5} />
             <Navbar />
             <Slider />
             {children}
